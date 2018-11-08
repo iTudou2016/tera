c879788ee3ba40d705d5c9c815d48780debd31ed
xxcc <34324426+iTudou2016@users.noreply.github.com>
2018-11-08 22:14:16 +0800

    Update aywa.sh

--- data/aywa.sh
+++ data/aywa.sh
@@ -16,3 +16,13 @@ export BDB_INCLUDE_PATH="/usr/local/BerkeleyDB.4.8/include"
 export BDB_LIB_PATH="/usr/local/BerkeleyDB.4.8/lib"
 ln -s /usr/local/BerkeleyDB.4.8/lib/libdb-4.8.so /usr/lib/libdb-4.8.so
 ln -s /usr/local/BerkeleyDB.4.8/lib/libdb_cxx-4.8.so /usr/lib/libdb_cxx-4.8.so
+
+sudo apt-get update
+sudo apt-get -y install python-virtualenv
+git clone https://github.com/GetAywa/Aywa_Masternode/ && cd Aywa_Masternode/sentinel
+virtualenv ./venv #(if locale.Error: unsupported locale setting: export LC_ALL=C)
+./venv/bin/pip install -r requirements.txt
+pwd
+
+# crontab -e
+# * * * * * cd /home/YOURUSERNAME/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1
