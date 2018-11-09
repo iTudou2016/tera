c3bf46143c4687c040442e89d619771747d6bc5f
xxcc <34324426+iTudou2016@users.noreply.github.com>
2018-11-09 22:02:28 +0800

    Update aywamn.sh

--- data/aywamn.sh
+++ data/aywamn.sh
@@ -16,19 +16,20 @@ mnip=$(curl ifconfig.me)
 cd aywa
 cp -r .aywa18 $mnfolder
 cd $mnfolder
-sudo sed -i 's/rpcport=/rpcport='$mnrpcport'/' aywa.conf
-sudo sed -i 's/port=/port='$mnport'/' aywa.conf
-sudo sed -i 's/rpcport=/'$mnrpcport'/' aywa.conf
-sudo sed -i 's/externalip=/externalip='$mnip'/' aywa.conf
-sudo sed -i 's/masternodeprivkey=/masternodeprivkey='$mngenkey'/' aywa.conf
-sudo sed -i 's/aywa18/'$mnfolder' '$mnip':'$mnport' '$mngenkey' '$mnoutputs' 1/' masternode.conf
+sudo sed -i 's/port=.*/port='$mnport'/' aywa.conf
+sudo sed -i 's/rpcport=.*/rpcport='$mnrpcport'/' aywa.conf
+sudo sed -i 's/externalip=.*/externalip='$mnip'/' aywa.conf
+sudo sed -i 's/masternodeprivkey=.*/masternodeprivkey='$mngenkey'/' aywa.conf
+sudo sed -i 's/aywa18.*/'$mnfolder' '$mnip':'$mnport' '$mngenkey' '$mnoutputs' 1/' masternode.conf
 
 sudo apt-get update
 sudo apt-get -y install python-virtualenv
 git clone https://github.com/GetAywa/Aywa_Masternode/ && cd Aywa_Masternode/sentinel
 virtualenv ./venv #(if locale.Error: unsupported locale setting: export LC_ALL=C)
 ./venv/bin/pip install -r requirements.txt
-sudo sed -i 's/aywa_conf=/aywa_conf=/root/aywa/'$mnfoler'/aywa.conf' sentinel.conf
+sudo sed -i 's/aywa_conf=.*/aywa_conf=/root/aywa/'$mnfolder'/aywa.conf' sentinel.conf
+./root/aywa/aywad -datadir=/root/aywa/$mnfolder
+pwd
 
-#crontab -e
-# * * * * * cd /home/YOURUSERNAME/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1
+echo 'crontab -e'
+echo '* * * * * cd /home/YOURUSERNAME/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1'
