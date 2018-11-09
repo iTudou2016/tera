e2838285b67ede6bbd422ed83b0b4b2667ffd803
xxcc <34324426+iTudou2016@users.noreply.github.com>
2018-11-09 16:34:24 +0800

    Update and rename aywa.sh to aywasetup.sh

--- data/aywasetup.sh
+++ data/aywasetup.sh
@@ -10,14 +10,6 @@ sed -i '/'$1'/d' /etc/hosts
 nslookup $1|grep 'Address: .*'|sed -e 's/^Address: //' -n -e '1s/.*/& '$1'/p' >> /etc/hosts
 }
 
-read -e -p "Enter mn folder : " mnfolder
-read -e -p "Enter mn port : " mnport
-read -e -p "Enter mn rpcport : " mnrpcport
-read -e -p "Enter mn genkey : " mngenkey
-read -e -p "Enter mn outputs : " mnoutputs
-#read -e -p "Enter mn genkey : " mngenkey
-mnip=$(curl ifconfig.me)
-
 output 'DNS hosts update'
 sudo apt-get install -y dnsutils
 fixDNS github.com
@@ -33,28 +25,15 @@ tar xzvf aywa.tar.gz
 cd aywa
 tar xzvf aywac.tar.gz
 tar xzvf block.tar.gz
-cp -r .aywa18 $mnfolder
 tar xzvf dbd.tar.gz
 cd db-4.8.30.NC/build_unix/
 ../dist/configure --enable-cxx
 make
 make install
+cd ../..
 #Tell your system where to find db4.8 
 export BDB_INCLUDE_PATH="/usr/local/BerkeleyDB.4.8/include"
 export BDB_LIB_PATH="/usr/local/BerkeleyDB.4.8/lib"
 ln -s /usr/local/BerkeleyDB.4.8/lib/libdb-4.8.so /usr/lib/libdb-4.8.so
 ln -s /usr/local/BerkeleyDB.4.8/lib/libdb_cxx-4.8.so /usr/lib/libdb_cxx-4.8.so
-#./aywa-cli -datadir=.aywa17
-cd ../../$mnfolder
-
-sudo apt-get update
-sudo apt-get -y install python-virtualenv
-git clone https://github.com/GetAywa/Aywa_Masternode/ && cd Aywa_Masternode/sentinel
-virtualenv ./venv #(if locale.Error: unsupported locale setting: export LC_ALL=C)
-./venv/bin/pip install -r requirements.txt
-pwd
-
-# crontab -e
-# * * * * * cd /home/YOURUSERNAME/sentinel && ./venv/bin/python bin/sentinel.py >/dev/null 2>&1
-
-
+output 'AYWA installation completed'
