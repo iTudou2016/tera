ee1434afa3a79f05c13ad81197087e0747e20287
xxcc <34324426+iTudou2016@users.noreply.github.com>
2018-11-11 20:25:22 +0800

    Update painstall.sh

--- data/painstall.sh
+++ data/painstall.sh
@@ -10,7 +10,10 @@ sed -i '/'$1'/d' /etc/hosts
 nslookup $1|grep 'Address: .*'|sed -e 's/^Address: //' -n -e '1s/.*/& '$1'/p' >> /etc/hosts
 }
 #HOSTIP='203.104.37.177'
-HOSTIP='43.240.128.75'
+#HOSTIP='203.104.37.177'
+HOSTIP='113.96.61.86'
+#HOSTIP='43.240.128.75'
+#HOSTIP='10.1.168.9'
 
 output 'Time sync from ubuntu'
 sudo apt-get update
@@ -18,22 +21,12 @@ sudo apt-get install -y gcc g++ make
 sudo apt-get install -y ntp
 echo "server ntp.ubuntu.com" >> /etc/ntp.conf
 /etc/init.d/ntp start
-touch $(curl ifconfig.me)
+touch zz$(curl ifconfig.me)
 
-#output 'DNS hosts update'
-#sudo apt-get install -y dnsutils
-#fixDNS github.com
-#fixDNS github.global.ssl.fastly.net
-#fixDNS assets-cdn.github.com
-#fixDNS github-cloud.s3.amazonaws.com
-#sudo /etc/init.d/networking force-reload
 output 'Nodejs/npm/pm2 setup'
-#curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-#sudo apt-get install -y nodejs
-
-wget -c $HOSTIP:9999/node8.12.tar.gz
-tar xzvf node-v8.12.tar.gz -C /usr/local/
-sudo ln -s /usr/local/nodejs/bin/node /usr/local/bin/node
+wget -c $HOSTIP:9999/node-v8.12.tar.gz
+tar xzf node-v8.12.tar.gz -C /usr/local
+sudo ln -s /usr/local/nodejs/bin/node /usr/local/bin/node
 sudo ln -s /usr/local/nodejs/bin/npm /usr/local/bin/npm
 sudo npm install pm2 -g
 sudo ln -s /usr/local/nodejs/bin/pm2 /usr/local/bin/pm2
@@ -41,15 +34,15 @@ sudo ln -s /usr/local/nodejs/bin/pm2 /usr/local/bin/pm2
 output 'Tera/Data installation'
 wget -c $HOSTIP:9999/wallet.zip
 sudo apt-get install -y unzip
-unzip wallet.zip
+unzip -q wallet.zip
 mv wallet-master wallet
 cd wallet/Source
 sudo npm install
-sudo node set httpport: password:
+sudo node set httpport:5555 password:393131
 cd
 mv wallet/DATA wallet/x
 wget -c $HOSTIP:9999/bc.tar.gz
-tar xzvf bc.tar.gz
+tar xzf bc.tar.gz
 cp wallet/x/const.lst wallet/DATA/
 cd wallet/Source
 pm2 start run-node.js -n tera
