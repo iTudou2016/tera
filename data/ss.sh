0f544102fc922bf31cbd2d8d5ac9b95f4f13bf76
xxcc <34324426+iTudou2016@users.noreply.github.com>
2018-11-24 21:01:23 +0800

    Update ss.sh

--- data/ss.sh
+++ data/ss.sh
@@ -1,19 +1,13 @@
-#更换ubuntu16.04源。 'bash ss.sh'
-#如果出错，reboot后重新运行 'sudo apt-get update'
-#完成后开始部署。 'bash tera.sh'
-touch zz$(curl ifconfig.me)
+#touch zz$(curl ifconfig.me)
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-echo "deb http://mirrors.ucloud.cn/ubuntu/ xenial main restricted universe multiverse
-deb http://mirrors.ucloud.cn/ubuntu/ xenial-security main restricted universe multiverse
-deb http://mirrors.ucloud.cn/ubuntu/ xenial-updates main restricted universe multiverse
-deb http://mirrors.ucloud.cn/ubuntu/ xenial-backports main restricted universe multiverse
-##测试版源
-deb http://mirrors.ucloud.cn/ubuntu/ xenial-proposed main restricted universe multiverse
-# 源码
-deb-src http://mirrors.ucloud.cn/ubuntu/ xenial main restricted universe multiverse
-deb-src http://mirrors.ucloud.cn/ubuntu/ xenial-security main restricted universe multiverse
-deb-src http://mirrors.ucloud.cn/ubuntu/ xenial-updates main restricted universe multiverse
-deb-src http://mirrors.ucloud.cn/ubuntu/ xenial-backports main restricted universe multiverse
-##测试版源
-deb-src http://mirrors.ucloud.cn/ubuntu/ xenial-proposed main restricted universe multiverse" > /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse" > /etc/apt/sources.list
 sudo apt-get update
