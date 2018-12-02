5099a7180cedee087ec1d6053fdf55c8fc79d5d7
xxcc <34324426+iTudou2016@users.noreply.github.com>
2018-12-02 10:58:44 +0800

    Create sucloud.sh

---
+++ data/sucloud.sh
@@ -0,0 +1,16 @@
+#touch zz$(curl ifconfig.me)
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "deb http://mirrors.ucloud.cn/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.ucloud.cn/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.ucloud.cn/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.ucloud.cn/ubuntu/ xenial-backports main restricted universe multiverse
+##测试版源
+deb http://mirrors.ucloud.cn/ubuntu/ xenial-proposed main restricted universe multiverse
+# 源码
+deb-src http://mirrors.ucloud.cn/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirrors.ucloud.cn/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirrors.ucloud.cn/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirrors.ucloud.cn/ubuntu/ xenial-backports main restricted universe multiverse
+##测试版源
+deb-src http://mirrors.ucloud.cn/ubuntu/ xenial-proposed main restricted universe multiverse" > /etc/apt/sources.list
+sudo apt-get update
